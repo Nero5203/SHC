f93c6c78d918b2de720089a4fbb4ddcc074b6344
Leka610 <qunileke@gmail.com>
2026-05-21 16:49:09 +0200

    Shtimi i entiteteve, enums dhe krijimi i entitetit te userit

---
+++ domain/Entities/Auth/UserCredential.cs
@@ -0,0 +1,15 @@
+using Domain.Entities.Users;
+namespace Domain.Entities.Auth
+{
+    public class UserCredentials
+    {
+        public Guid Id { get; set; }
+        public string PasswordHash { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+
+    }
+}
---
+++ domain/Entities/User/User.cs
@@ -0,0 +1,18 @@
+using Domain.Entities.Auth;
+namespace Domain.Entities.User
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? ProfilePictureUrl { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public UserCredentials Credentials { get; set; } = null!;
+    }
+}
