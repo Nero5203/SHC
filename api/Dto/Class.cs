51d9591ed8ea4c4d79f3cf22cb8b857f4ba97dd3
Lukë Muqaj <lm68232@ubt-uni.net>
2026-05-21 15:31:49 +0200

    Test push

---
+++ api/Dto/Class.cs
@@ -0,0 +1,6 @@
+﻿namespace api.Dto
+{
+    public class Class
+    {
+    }
+}
---
+++ ports/DrivingPorts/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ports.DrivingPorts
+{
+    internal class Class1
+    {
+    }
+}
