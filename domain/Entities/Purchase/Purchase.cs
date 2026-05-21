28613f8b558fce38a41146a9f1c02f0b1c3554d6
Gabriel <gabrielmarjakaj@gmail.com>
2026-05-21 18:33:35 +0200

    small changes in purchase

--- domain/Entities/Purchase/Purchase.cs
+++ domain/Entities/Purchase/Purchase.cs
@@ -10,11 +10,16 @@ namespace Domain.Entities.Purchase
         public Guid UserId { get; set; }
         public UserEntity User { get; set; } = null!;
 
+        public Guid? SubscriptionId { get; set; }
+        public Subscription? Subscription { get; set; }
+
         public decimal Amount { get; set; }
         public string Currency { get; set; } = "EUR";
 
         public DateTime PurchasedAt { get; set; }
 
         public PurchaseStatus Status { get; set; }
+
+        public Invoice? Invoice { get; set; }
     }
 }
