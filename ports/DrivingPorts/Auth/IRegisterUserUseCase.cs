b005406b6f9991ba8c7b018707b9a5deb84cb317
Gabriel <gabrielmarjakaj@gmail.com>
2026-06-04 22:35:07 +0200

    deleted ports forler outside app(duplicate

--- ports/DrivingPorts/Auth/IRegisterUserUseCase.cs
+++
@@ -1,8 +0,0 @@
-using Application.Dtos.Auth;
-namespace ports.DrivingPorts.Auth
-{
-    public interface IRegisterUserUseCase
-    {
-        Task RegisterUserAsync(RegisterDto registerDto);
-    }
-}
